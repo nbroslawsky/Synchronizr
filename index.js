91b81eae474a4e7bfe41c4c39c7bbe670d8ebc49
Nathan Broslawsky <nathan.broslawsky@nsfive.com>
2012-06-04 20:12:50 -0600

    Added initial files

---
+++ index.js
@@ -0,0 +1,26 @@
+var net = require('net'),
+	config = require('./config.json');
+
+net.createServer(function(socket) {
+	socket.name = socket.remoteAddress + ":" + socket.remotePort;
+
+	socket.write("Hello " + socket.name + ". Welcome.");
+
+	var data = [];
+	
+	socket.on('data', function(d) { data.push(d); });
+
+	socket.on('end', function() {
+		var result,
+			s = data.join('');
+		try {
+			result = JSON.parse(s) || null;
+		} catch(e) {
+			result = s;
+		}
+
+		if(result) {
+			console.log(result);
+		}
+	});
+}).listen(config.port || 1337);
---
+++ test.js
@@ -0,0 +1,82 @@
+var net = require('net'),
+	fs = require('fs'),
+	path = require('path'),
+	spawn = require('child_process').spawn;
+
+
+var directory = '/home/nbroslawsky/test',
+	cmd = 'inotifywait',
+	args = ['-q','-e','modify','-e','attrib','-e','create','-e','delete','-e','move','--format','"%w%f"','-mr', directory],
+	inotify = spawn(cmd, args);
+
+
+inotify.stdout.on('data', function(data) {
+	console.log(JSON.parse(data));
+});
+
+inotify.stderr.on('data', function(data) {});
+
+inotify.on('exit', function(data) {
+	console.log(data, 'the process exited');
+});
+
+/*
+fs.watch('/home/nbroslawsky/test', function(event, filename) {
+	if(filename) {
+		console.log(event, 'filename provided: ' + filename);
+	} else {
+		console.log('filename not provided');
+	}
+});
+*/
+
+/*
+function getHandler(directory) {
+	return function(e, filename) {
+		console.log('directory',directory,', filename',filename);
+		var fullPath = path.join(directory,filename),
+			isDir = fs.statSync(fullPath).isDirectory();
+
+		if(isDir && !directories[fullPath]) {
+			console.log("now watching " + fullPath);
+			directories[fullPath] = watch(fullPath);
+		} else if(!isDir && directories[fullPath]) {
+			console.log("directory no longer exists: " + fullPath, Object.keys(directories[fullPath]));
+			delete directories[fullPath];
+		} else {
+			console.log("Don't need to do anything with " + fullPath);
+		}
+	}
+}
+
+var directories = {};
+function watch(directory) {
+
+	fs.watch(directory, getHandler(directory));
+
+	var thisPath, files = fs.readdirSync(directory);
+	for(var i=0; i<files.length; i++) {
+		thisPath = path.join(directory, files[i]);
+		if(fs.statSync(thisPath).isDirectory()) {
+			console.log(thisPath + " is a directory");
+			directories[thisPath] = watch(thisPath);
+		}
+	}
+}
+
+watch('/home/nbroslawsky/test');
+*/
+
+
+
+/*
+var client = net.connect(1337, function() {
+	fs.fstat('/home/nbroslawsky/Sublime Text 2/sublime_text_icon_2181.png', function(err, data) {
+		if(err) {
+			console.log(err);
+			return;
+		}
+		client.write(JSON.stringify(data));
+		client.end();
+	})
+})*/
