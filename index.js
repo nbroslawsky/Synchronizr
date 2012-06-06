1ef889fa2a64d1796d4676c99cf185fcb24c993c
Nathan Broslawsky <nathan.broslawsky@nsfive.com>
2012-06-05 21:40:01 -0600

    Finished the tar transfers

--- index.js
+++ index.js
@@ -2,35 +2,15 @@ var net = require('net'),
 	utils = require('./lib/utils.js'),
 	config = require('./config.json');
 
+var args = require('optimist').argv;
+['master','port'].forEach(function(val) {
+	if(args[val]) {
+		config[val] = args[val];
+	}
+})
 
 var actor = (config.master)
 	? require('./lib/actors/slave.js')
 	: require('./lib/actors/master.js');
 
 actor.init(config);
-
-
-/*
-net.createServer(function(socket) {
-	socket.name = socket.remoteAddress + ":" + socket.remotePort;
-
-	socket.write("Hello " + socket.name + ". Welcome.");
-
-	var data = [];
-
-	socket.on('data', function(d) { data.push(d); });
-
-	socket.on('end', function() {
-		var result,
-			s = data.join('');
-		try {
-			result = JSON.parse(s) || null;
-		} catch(e) {
-			result = s;
-		}
-
-		if(result) {
-			console.log(result);
-		}
-	});
-}).listen(config.port || 1337);*/
